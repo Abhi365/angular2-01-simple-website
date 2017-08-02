cf562034de88ca999f11eefb4477e8ee2e9a8793
Bansal <abhishek.b.bansal@capgemini.com>
2017-08-02 09:12:53 +0530

    initial commit

---
+++ src/app/routing.ts
@@ -0,0 +1,10 @@
+import { Router , RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+export const routing = RouterModule.forRoot([
+    { path: '', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: '**', component: HomeComponent},
+]);
